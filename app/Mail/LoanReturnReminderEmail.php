a639c83e58795c9532f4e537c06b6b4ee6a33183
José Briceño <josejorgebriceno9@gmail.com>
2024-03-18 00:20:25 -0400

    Agregado markdown para recordatorio de devoluciones.

---
+++ app/Mail/LoanReturnReminderEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class LoanReturnReminderEmail extends Mailable
+{
+   
+    use Queueable, SerializesModels;
+
+    public $loanDetails;
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Recordatorio de devolución de préstamo',
+        );
+    }
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($loanDetails)
+    {
+        $this->loanDetails = $loanDetails;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Recordatorio: Pronto debe devolver su préstamo (ID: ' . $this->loanDetails['id'] . ')')
+                    ->markdown('emails.loan-return-reminder', $this->loanDetails);
+    }
+}
---
+++ resources/views/vendor/mail/loan-return-reminder.php
@@ -0,0 +1,23 @@
+## Loan Return Reminder (ID: {{ $loanDetails['id'] }})
+
+Dear {{ $loanDetails['user']['name'] }},
+
+This is a friendly reminder that your loan (ID: {{ $loanDetails['id'] }}) is due for return tomorrow, on {{ Carbon::parse($loanDetails['delivery_date'])->addDay()->format('Y-m-d') }}.
+
+Please ensure you return the loaned items to us by the end of the day tomorrow.  
+
+**Here are some ways to return your loan:**
+
+* **Bring it back to our store:** You can return the loaned items in person to our store located at [Store Address]. Our store hours are [Store Hours].
+* **Arrange for pickup (if available):** If pickup is available in your area, you can contact us to schedule a convenient time for pickup.
+
+**For any questions or concerns regarding your loan return, please don't hesitate to contact us:**
+
+* **Email:** [Your Email Address]
+* **Phone:** [Your Phone Number]
+
+Thank you for your business!
+
+Sincerely,
+
+The [Your Company Name] Team
